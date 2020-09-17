24c2abfbce3a52b35c9501a5aa6f2d206951b0c2
erfanTempolar <sherlockholmz57@gmail.com>
2020-09-16 23:02:28 -0700

    just front

---
+++ js/components/user/template/footer/mission/mission.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="mission">
+		<p>هدف ما در دمیرکو آسان کردن خرید صنعتی و ایجاد اعتماد بین خریدار و فروشنده است.و در ززمینه فروش جت پرینتر,شیرینگ پک,خط تولید لنک,خط تولید سیم و کابل و .... آماده خدمت  رسانی هستیم.</p>
+		<div id="diploma">
+			<img src="/images/etehad.png" alt="">
+			<img src="/images/senfi.png" alt="">
+			<img src="/images/saman.png" alt="">
+		</div>
+	</div>
+</template>
+
+
+<style scoped>
+	#mission{
+		margin:20px;
+	}
+	img{
+		width:80px;
+	}
+p{
+	width: 300px;
+	text-align: justify;
+	line-height: 1.2rem
+}
+	</style>
