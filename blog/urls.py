26e9961adebb6f934a378d1d1a30d95b01da2e4f
Alireza Alavi Razavi <alirezaalavirazavi@yahoo.com>
2020-09-20 06:41:24 -0400

    pagination fix

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.post_list_view),
     path('posts/', views.post_list),
     path('post/<slug>/', views.post_detail),
-    path('posts/<int:page>', views.paginated_post),
+    path('posts/page/<int:page>', views.paginated_post),
 ]
