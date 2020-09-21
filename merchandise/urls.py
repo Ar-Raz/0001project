b63ced037080d1b2bd082a2ab544e4acb601c3ea
Alireza Alavi Razavi <alirezaalavirazavi@yahoo.com>
2020-09-21 03:23:47 -0400

    product_name02

--- merchandise/urls.py
+++ merchandise/urls.py
@@ -5,5 +5,5 @@ app_name = 'merchandise'
 from .views import MiniOrderView
 
 urlpatterns = [
-    path('miniorder/<slug>', MiniOrderView.as_view(), name="miniorder"),
+    path('miniorder', MiniOrderView.as_view(), name="miniorder"),
 ]
